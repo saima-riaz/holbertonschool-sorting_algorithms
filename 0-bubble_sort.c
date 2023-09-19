682f4ce909c24cb3371be7c53b561e4306deaf5b
saima-riaz <saimariaz1422@gmail.com>
2023-09-19 08:15:46 -0700

    task 0 array of integers in ascending order using the Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort
+ * @arry: array of integers
+ * @size: size of arry
+ *
+ * Return: None
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, p1,p2;
+	siez_t size2 = size;
+
+	if (array == NULL || size == 0 || size < 2)
+		return;
+	for (i = 0; i <= size - 1; i += 1)
+	{
+		for (j = 0; j < size2 - i - 1; j += 1)
+		{
+			p1 = array[j];
+			p2 = array[j + 1];
+			if (p1 > p2)
+			{
+				int temp = p1;
+				p1 = p2;
+				p2 = temp;
+				array[j] = p1;
+				array[j + 1] = p2;
+				print_array(array, size);
+			}
+		}
+	}
+}
